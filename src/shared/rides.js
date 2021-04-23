d8cb39e81f939f4cda5148ac36ad6868e2413984
David Vernall <davidvernall@hotmail.com>
2021-04-23 15:00:25 +0100

    more setup, directory, main and header

---
+++ src/shared/rides.js
@@ -0,0 +1,44 @@
+export const RIDES = [
+    {
+        id: 0,
+        name: "Boulder Canyon",
+        image: "/assets/images/19DC36D8-CF84-4583-AE1A-0E2E46DC3B48_1_105_c.jpeg",
+        featured: false,
+        description: "This is a long ride with huge elevation gains"
+    },
+    {
+        id: 1,
+        name: "Boulder - Lyons",
+        image: "/assets/images/8A06ABCF-C54E-443F-978D-CA67BCF776BD_1_105_c.jpeg",
+        featured: false,
+        description: "This is a long ride with plenty of long flats"
+    },
+    {
+        id: 2,
+        name: "Boulder Resevoir",
+        image: "/assets/images/27C5A8AF-C49D-4AF4-95ED-B1023BA5A445_1_105_c.jpeg",
+        featured: false,
+        description: "This is a medium ride with little elevation"
+    },
+    {
+        id: 3,
+        name: "NCAR",
+        image: "/assets/images/collin-armstrong-M00Wcee-oRw-unsplash.jpg",
+        featured: false,
+        description: "This is a quick ride with steep elevation gains"
+    },
+    {
+        id: 4,
+        name: "Left Hand Canyon",
+        image: "/assets/images/malachi-brooks-TmOnZVOqrAc-unsplash.jpg",
+        featured: false,
+        description: "This is a long, sustained mountain climb"
+    },
+    {
+        id: 5,
+        name: "Lee Hill Loop",
+        image: "/assets/images/781C9862-E1A0-4982-8F69-603C5E5A4066_1_105_c.jpeg",
+        featured: false,
+        description: "Two hour ride with steep up and downs"
+    },
+];
